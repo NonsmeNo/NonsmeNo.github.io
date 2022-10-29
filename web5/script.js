9031ec41e3d58463ca36f3f6e9b113fc176cf2a4
NonsmeNo <79172122+NonsmeNo@users.noreply.github.com>
2022-10-29 22:11:18 +0300

    Update script.js

--- web5/script.js
+++ web5/script.js
@@ -1,18 +1,19 @@
-const f1 = document.getElementsByName("field1");
-const f2 = document.getElementsByName("field2");
-const r = document.getElementById("result");
-const btn = document.querySelector('button');
+window.addEventListener('DOMContentLoaded', function() {
+    const f1 = document.getElementsByName("field1");
+    const f2 = document.getElementsByName("field2");
+    const r = document.getElementById("result");
+    const btn = document.querySelector('button');
 
-const reg = /\D/;
+    const reg = /\D/;
 
-function sum() {
-    if(reg.test(f1[0].value) || reg.test(f2[0].value))
-        r.textContent = 'Неверный формат';
-    else {
-        r.textContent = f1[0].value * f2[0].value; 
-        console.log('!');
-    }
-}
+    function sum() {
+        if(reg.test(f1[0].value) || reg.test(f2[0].value))
+            r.textContent = 'Неверный формат';
+        else {
+            r.textContent = f1[0].value * f2[0].value; 
+            console.log('!');
+        }
+});
 
 
 btn.addEventListener('click', function () {
