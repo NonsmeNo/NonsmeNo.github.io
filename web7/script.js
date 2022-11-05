9f6dea9a9f71b5048bdc5d8d2b362d3bf0721891
NonsmeNo <79172122+NonsmeNo@users.noreply.github.com>
2022-11-05 21:03:53 +0300

    Update script.js

--- web7/script.js
+++ web7/script.js
@@ -5,6 +5,8 @@ $(function () {
         slidesToShow: 4,
         slidesToScroll: 1,
         speed: 300,
+        autoplay: true, 
+        autoplaySpeed: 1000,
         responsive: [
             {
               breakpoint: 768,
