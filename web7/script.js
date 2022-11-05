131942984d67c87b192c84570c9d3b0d0350adc2
NonsmeNo <79172122+NonsmeNo@users.noreply.github.com>
2022-11-05 18:19:44 +0300

    Update script.js

--- web7/script.js
+++ web7/script.js
@@ -1,1 +1,17 @@
-
+$(function () {
+    $('.slider').slick({
+        infinite: true,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        speed: 300,
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2
+              }
+            }
+          ]
+      })
+})
